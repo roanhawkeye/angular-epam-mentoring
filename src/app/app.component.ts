f0667c003a885d5b22506d5a50cb49b94033c3bd
Daniel Martinez <roanhawkeye@gmail.com>
2024-01-20 14:17:12 -0600

    [task-2] - clean app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from "./card/card.component";
-import { Card } from './models/Card';
 import { CardService } from './services/card.service';
 
 @Component({
@@ -10,15 +8,9 @@ import { CardService } from './services/card.service';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    providers: [CardService],
     imports: [CommonModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'epam-mentoring-angular';
-  cards: Card[];
-
-  constructor(private cardService: CardService) {
-    this.cards = cardService.getAllCards();
-  }
 
 }
